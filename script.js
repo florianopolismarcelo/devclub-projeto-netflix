de38038d735068a2666046a3b35d57d5f97de90e
florianopolismarcelo <florianopolismarcelo@msn.com>
2024-10-22 11:33:08 -0300

    devclube projeto netflix

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ let botaoSom = document.querySelector(".botao-som");
 let video = document.querySelector(".video");
 let botao = document.querySelector(".link-info");
 let modal = document.querySelector(".modal");
+let audio = document.querySelector(".audio");
 
 // Essa parte do código é para ligar o som
 botaoSom.addEventListener("click", ligaSom);
 
 function ligaSom() {
   //alert("Opa! O botão foi clicado")
-  video.muted = false;
+  video.muted = !video.muted // Código para ligar e desligar o som 
 }
 
 // Essa parte do código é para mostrar modal
@@ -23,3 +24,48 @@ function mostrarModal() {
 function esconderModal() {
   modal.style.display = "none";
 }
+
+//Tocar o TUNDUM
+
+window.addEventListener("load", tocarAudio);
+function tocarAudio() {
+  audio.play();
+}
+
+
+
+/*
+// Uma outra forma de fazer o código do TUNDUM acima
+
+const audioOutraFormaDeFazer = document.querySelector(".audio");
+window.addEventListener("load", () => {
+  audioOutraFormaDeFazer.play();
+});
+*/
+
+
+/*
+// Abaixo: é uma  forma de fazer o código do TUNDUM acima
+
+
+e tem como fazer ele sem precisar colocar o caminho do Audio no HTML
+voce so cria uma variavel ai no JS
+declara a musica
+
+const musica = new Audio('./src/audio/tudum-netflix-sound.mp3')
+
+e faz a função para ela tocar com botão ou sem
+
+
+musicaFocoInput.addEventListener('change', ()=> {
+  if(musica.paused) {
+      musica.play()
+  }else {
+      musica.pause()
+  }
+})
+
+nesse meu caso estou usando aqueles botoes de mover para o lado
+
+*/
+
